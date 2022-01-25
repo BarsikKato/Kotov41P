a2018111ef8288635b61f0707cb78e660c205ec7
Barsik Kato <noran-kotov@yandex.ru>
2022-01-25 08:32:01 +0300

    Условное форматирование

---
+++ ProektRes/UsersPartial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotov_ProektRes
+{
+    public partial class users
+    {
+        public bool isBoy { get => gender == 1; }
+        public bool isGirl { get => gender == 2; }
+        public bool isAlien { get => gender == 3; }
+    }
+}
